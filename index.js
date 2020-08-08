9f801cf0947811a9b30993e18d7cde5644b1ba60
Daniel Neves <dbenditoneves@gmail.com>
2020-08-08 18:13:51 -0300

    MVP 3 finalizado

--- index.js
+++ index.js
@@ -31,23 +31,62 @@ function ObterMsg(){
             let h1 = document.createElement("h1");
             let p =  document.createElement("p");
             let btn = document.createElement("button");
+            let edit = document.createElement("button");
             h1.innerHTML = response[i].name;
             p.innerHTML = response[i].message;
-            btn.innerHTML = "DELETAR";
+            btn.innerHTML = "DELETAR"; //Segunta parte(MVP 2-Botão deletar)
+            edit.innerHTML = "Editar mensagem";
             card.appendChild(h1);
             card.appendChild(p);
             card.appendChild(btn);
+            card.appendChild(edit);
             card.id = response[i].id;
             Pessoas.appendChild(card);
 
             btn.addEventListener("click", function(){
                 fetch("https://treinamentoajax.herokuapp.com/messages" + "/" + card.id, {method:"DELETE"}).then(response => {ObterMsg()})
             })
+//Terceira parte(MVP 3-Botão editar)
+            edit.addEventListener("click", function(){
+                var x = document.createElement("TEXTAREA");
+                var y = document.createElement("TEXTAREA");
+                let confirmedit = document.createElement("button");
+                let cancelar = document.createElement("button");
+                confirmedit.innerHTML = "Editar";
+                cancelar.innerHTML = "Cancelar";
+                card.appendChild(x);
+                card.appendChild(y);
+                card.appendChild(confirmedit);
+                card.appendChild(cancelar);
+
+                confirmedit.addEventListener("click", function(){
+                    var fetchBody = {
+                        "message":{
+                            "name":x.value,
+                            "message":y.value
+                        }
+                    }
+                    var fetchConfig = {
+                        method: "PUT",
+                        headers: {"Content-Type":"application/JSON"},
+                        body: JSON.stringify(fetchBody)
+                        }
+                    fetch("https://treinamentoajax.herokuapp.com/messages" + "/" + card.id, fetchConfig)
+                })
+
+                cancelar.addEventListener("click", function(){
+                    x.remove();
+                    y.remove();
+                    confirmedit.remove();
+                    cancelar.remove();
+
+                })
+            })
         }
     })
 }
 
-//Segunda parte (MVP 2)
+//Segunda parte (MVP 2-Obter Mensagem específica)
 
 function ObterMsgespecífica(){
     Pessoas.innerHTML = ""
@@ -60,17 +99,56 @@ function ObterMsgespecífica(){
                 let h1 = document.createElement("h1");
                 let p =  document.createElement("p");
                 let btn = document.createElement("button");
+                let edit = document.createElement("button");
                 h1.innerHTML = response[i].name;
                 p.innerHTML = response[i].message;
                 btn.innerHTML = "DELETAR";
+                edit.innerHTML = "Editar mensagem";
                 card.appendChild(h1);
                 card.appendChild(p);
                 card.appendChild(btn);
+                card.appendChild(edit);
                 card.id = response[i].id;
                 Pessoas.appendChild(card);
 
                 btn.addEventListener("click", function(){
                     fetch("https://treinamentoajax.herokuapp.com/messages" + "/" + card.id, {method:"DELETE"}).then(response => {ObterMsg()})
+                })
+
+                edit.addEventListener("click", function(){
+                    var x = document.createElement("TEXTAREA");
+                    var y = document.createElement("TEXTAREA");
+                    let confirmedit = document.createElement("button");
+                    let cancelar = document.createElement("button");
+                    confirmedit.innerHTML = "Editar";
+                    cancelar.innerHTML = "Cancelar";
+                    card.appendChild(x);
+                    card.appendChild(y);
+                    card.appendChild(confirmedit);
+                    card.appendChild(cancelar);
+    
+                    confirmedit.addEventListener("click", function(){
+                        var fetchBody = {
+                            "message":{
+                                "name":x.value,
+                                "message":y.value
+                            }
+                        }
+                        var fetchConfig = {
+                            method: "PUT",
+                            headers: {"Content-Type":"application/JSON"},
+                            body: JSON.stringify(fetchBody)
+                            }
+                        fetch("https://treinamentoajax.herokuapp.com/messages" + "/" + card.id, fetchConfig)
+                    })
+    
+                    cancelar.addEventListener("click", function(){
+                        x.remove();
+                        y.remove();
+                        confirmedit.remove();
+                        cancelar.remove();
+    
+                    })
                 })
             }
         }
